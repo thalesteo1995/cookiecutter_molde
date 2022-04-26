a2b7f993b7792f489080eaf0a866020717cd3aa2
Thales Teodoro Ampere <thales@ampereconsultoria.com.br>
2022-04-25 21:23:47 -0300

    fix: reajustando o setup

---
+++ {{cookiecutter.directory_name}}/main.py
@@ -0,0 +1,11 @@
+"""
+    Description:
+
+    Author:           {{cookiecutter.author_name}}
+    Created:
+"""
+def main():
+    print("Hello, {{cookiecutter.author_name}}!")
+
+if __name__ == "__main__":
+    main()
